a14be6675552bcce4634b72dfe5653dd5eb048fb
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-10-31 14:04:23 -0400

    Git merge Ariel PR

--- obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db93d68cf98796941f7e32791f46148d7420ecf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b02d8e329ab22a3c54312e7b2437381abde2f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

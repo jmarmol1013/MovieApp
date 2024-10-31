4ebba58f308f091d30e6b8847b73f6d25f93e61c
ariel <ariel@ARIELZHOU>
2024-10-31 11:16:04 -0400

    code

--- obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed864c0a95411ecab4d43b822783f1abdf53015c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ab807c1ef23f2233611eb1f1a23d2385b4c87b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
